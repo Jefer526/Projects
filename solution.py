8ac37c667072f44b3217bf21597e6fb3fc4e304c
Jefer526 <Jefer5261@gmail.com>
2025-02-19 19:49:38 -0500

    Hangman

---
+++ solution.py
@@ -0,0 +1,39 @@
+import random
+
+word_list = ["aardvark","baboon", "camel"]
+
+#Random word
+chosen_word = (random.choice (word_list))
+print (chosen_word)
+
+#Places in the word
+placeholder = ""
+word_length = len(chosen_word)
+for position in range (word_length):
+    placeholder += "_"
+print (placeholder)
+
+
+#Ask user a letter
+game_over = False
+correct_letters = []
+
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else :
+            display += "_"
+
+    print (display)
+
+    if "_" not in display :
+        game_over = True
+        print ("You win")
